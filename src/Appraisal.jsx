94f6850fac1e197854f31c9ff5fd205e433de4c3
breakfastmeansbreakfast <77744c@gmail.com>
2018-12-06 22:03:00 +0000

    total revenue working for interim rents

--- src/Appraisal.jsx
+++ src/Appraisal.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const _ = require('lodash');
 
 const calcInputs = {
   rents: {
@@ -24,7 +26,7 @@ const calcInputs = {
 const rentArray = (unitInput) => {
   const rentarray = [];
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i <= (calcInputs.growthPeriod + calcInputs.stabalisationPeriod); i++) {
+  for (let i = 0; i <= (calcInputs.growthPeriod + calcInputs.stabalisationPeriod - 1); i++) {
     let currentmonthrent = 1;
     currentmonthrent = unitInput * Math.pow((1 + calcInputs.growthRate / 12), (i));
     rentarray.push(currentmonthrent);
@@ -38,19 +40,43 @@ const rentArray = (unitInput) => {
 // console.log(rentArray(calcInputs.rents.rent3bed));
 
 // get rent array for sum to calculate. pass in rantArray(1bed / 2bed) to get grown results
-const rentTotal = (rentArrayP) => {
+const rentTotal = (unitType, unitTypeNo) => {
   const rentTotalArray = [];
-  for (let i = calcInputs.growthPeriod; i <= (calcInputs.growthPeriod + calcInputs.stabalisationPeriod); i++) {
-    rentTotalArray.push(rentArrayP[i]);
+  const rentroll = [];
+  for (let i = calcInputs.growthPeriod; i <= (calcInputs.growthPeriod + calcInputs.stabalisationPeriod - 1); i++) {
+    rentTotalArray.push(unitType[i]);
+    rentroll.push(unitType[i] * unitTypeNo);
+  }
+  const rentTotalAmount = _.sum(rentroll);
+  const rentData = {
+    rentTotalArray: rentTotalArray,
+    rentroll: rentroll,
+    rentTotalAmount: rentTotalAmount,
   };
-  return rentTotalArray;
+  return rentData;
 };
 
 // create array of full rents from month 0 onwards
 const oneBedArray = rentArray(calcInputs.rents.rent1bed);
+const twoBedArray = rentArray(calcInputs.rents.rent2bed);
+const threeBedArray = rentArray(calcInputs.rents.rent3bed);
 
 // create array of rents to be valued
-console.log(rentTotal(oneBedArray));
+// console.log(rentTotal(oneBedArray, calcInputs.units.bedno1));
+
+// see https://www.linkedin.com/pulse/javascript-find-object-array-based-objects-property-rafael
+const interimRevenue = () => {
+  const interim1bed = rentTotal(oneBedArray, calcInputs.units.bedno1);
+  const interim2bed = rentTotal(twoBedArray, calcInputs.units.bedno2);
+  const interim3bed = rentTotal(threeBedArray, calcInputs.units.bedno3);
+
+  const total = interim1bed.rentTotalAmount + interim2bed.rentTotalAmount + interim3bed.rentTotalAmount
+
+  return (total);
+};
+
+console.log(interimRevenue());
+
 
 // get inputs and calculate profit
 const calc = () => {
@@ -68,7 +94,6 @@ const calc = () => {
 
   // need to get interim rev to match *next job
   const interimRevenue = ((rentRevenue)) * (calcInputs.stabalisationPeriod); // not ok
-  console.log(interimRevenue);
 
   const totalRevenue = (stabalisedRevenue + interimRevenue);
   return totalRevenue.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
